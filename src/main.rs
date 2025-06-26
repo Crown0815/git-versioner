9cef40f2bcb56aca8dde5a3da196a9d4e49acf5a
Crown0815 <felix.kroener@gmx.de>
2025-06-26 11:57:44 -0500

    refactor: Remove comments and unsued imports from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::Parser;
 use git_versioner::GitVersioner;
 use std::path::PathBuf;
 
-/// Git Versioner - Automatically calculate version numbers for trunk-based development
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Path to the Git repository (defaults to current directory)
     #[clap(short, long, value_parser)]
     repo_path: Option<PathBuf>,
 
-    /// Print detailed version information
     #[clap(short, long)]
     verbose: bool,
 }
@@ -19,10 +16,8 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    // Use current directory if no path is specified
     let repo_path = args.repo_path.unwrap_or_else(|| std::env::current_dir().unwrap());
 
-    // Calculate the version
     let versioner = GitVersioner::new(&repo_path)?;
     let version = versioner.calculate_version()?;
 
