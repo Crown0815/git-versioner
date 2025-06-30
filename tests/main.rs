69e599aa0950d78812aa74e57d16f951d501b210
Crown0815 <felix.kroener@gmx.de>
2025-06-30 17:55:25 -0500

    refactor: Simplify tests against CLI

--- tests/main.rs
+++ tests/main.rs
@@ -1,56 +1,42 @@
-mod lib_integration;
-use lib_integration::TestRepo;
+mod lib;
+
+use lib::TestRepo;
 use rstest::{fixture, rstest};
+use std::path::Path;
 
 use assert_cmd::Command;
 
+const TRUNK: &str = "trunk";
+
 #[fixture]
-fn repo(
-    #[default("trunk")] main: &str
-) -> TestRepo {
+fn repo(#[default(TRUNK)] main: &str) -> TestRepo {
     let repo = TestRepo::new();
     repo.initialize(main);
-
     repo.commit("0.1.0-rc.1");
-    repo.branch("feature/feature1");
-    repo.commit("0.1.0-feature1.1");
-
-    repo.checkout(main);
-    repo.merge("feature/feature1");
-    repo.tag("v1.0.0");
-    repo.branch("release/1.0.0");
-
-    repo.checkout(main);
-    repo.branch("feature/feature2");
-    repo.commit("1.1.0-feature2.1");
-    repo.commit("1.1.0-feature2.2");
-
-    repo.checkout(main);
-    repo.merge("feature/feature2");
-
-    repo.checkout("release/1.0.0");
-    repo.commit("1.0.1-rc.1");
-    repo.branch("feature/fix1");
-    repo.commit("1.0.1-fix1.1");
-    repo.commit("1.0.1-fix1.2");
-    repo.checkout("release/1.0.0");
-    repo.merge("feature/fix1");
-
     repo
 }
 
 #[rstest]
-fn test_custom_main_branch_name(#[with("custom-trunk")] repo: TestRepo) {
-    repo.checkout("custom-trunk");
-    assert_version(&repo, "1.1.0-rc.3", "custom-trunk")
+fn test_default_main_branch_name(repo: TestRepo) {
+    assert_version(&repo.path, &[], "0.1.0-rc.1")
 }
 
-fn assert_version(repo: &TestRepo, expected: &str, main_branch: &str) {
+#[rstest]
+fn test_custom_main_branch(#[with("custom-main")] repo: TestRepo) {
+    assert_version(&repo.path, &["--main-branch", "custom-main"], "0.1.0-rc.1")
+}
+
+#[rstest]
+fn test_repository_not_in_working_directory(repo: TestRepo) {
+    assert_version(".", &["--repo-path", repo.path.to_str().unwrap()], "0.1.0-rc.1")
+}
+
+fn assert_version<P: AsRef<Path>>(cd: P, args: &[&str], expected: &str) {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))
         .expect("CLI binary not found");
 
-    cmd.args(&["--main-branch", main_branch])
-        .current_dir(&repo.path)
+    cmd.args(args)
+        .current_dir(cd)
         .assert()
         .success()
         .stdout(format!("{expected}\n"));
