3a4742b46297d811f7a7740e8183568018c0d8b6
Crown0815 <felix.kroener@gmx.de>
2025-07-06 20:16:57 -0500

    test: Ensure cli overrides config as expected

--- tests/cli_with_configuration.rs
+++ tests/cli_with_configuration.rs
@@ -36,3 +36,14 @@ fn test_release_candidate_on_toml_configured_main_branch(
 
     assert_configured_repo_cmd_snapshot!(repo, config_file, cli.current_dir(&repo.path));
 }
+
+#[rstest]
+fn test_release_candidate_on_toml_configured_and_cli_overridden_main_branch(
+    #[with(CUSTOM_MAIN_BRANCH)] mut repo: TestRepo,
+    mut cli: Command
+) {
+    repo.cli_config.main_branch = Some(format!("^{}$", "another_main_branch"));
+    let config_file = repo.create_default_toml_config();
+
+    assert_configured_repo_cmd_snapshot!(repo, config_file, cli.current_dir(&repo.path).args(&["--main-branch", CUSTOM_MAIN_BRANCH]));
+}
