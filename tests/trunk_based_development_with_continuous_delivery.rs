215352018743f9d650b2c64a6bed2a835886c552
Crown0815 <felix.kroener@gmx.de>
2025-12-29 22:02:32 +0100

    refactor: Remove unnecessary mutability

--- tests/trunk_based_development_with_continuous_delivery.rs
+++ tests/trunk_based_development_with_continuous_delivery.rs
@@ -45,14 +45,13 @@ fn test_that_with_custom_pre_release_tag_when_non_matching_tags_exist_produces_p
 }
 
 #[rstest]
-fn test_that_when_no_tags_exist_produces_pre_release_tag_1(mut repo: TestRepo) {
+fn test_that_when_no_tags_exist_produces_pre_release_tag_1(repo: TestRepo) {
     repo.commit("0.1.0+2");
-
     repo.assert().version("0.1.0-pre.1").version_source_sha("");
 }
 
 #[rstest]
-fn test_that_when_matching_tags_exist_produces_next_pre_release_tag(mut repo: TestRepo) {
+fn test_that_when_matching_tags_exist_produces_next_pre_release_tag(repo: TestRepo) {
     let (sha, _) = repo.tag("v0.1.0-pre.1");
     repo.commit("0.1.0+2");
     repo.commit("0.1.0+3");
@@ -64,7 +63,7 @@ fn test_that_when_matching_tags_exist_produces_next_pre_release_tag(mut repo: Te
 
 #[rstest]
 fn test_that_after_release_when_no_matching_tags_exist_produces_pre_release_tag_1(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     let (sha, _) = repo.tag("v1.0.0");
     repo.commit("1.1.0+1");
@@ -77,7 +76,7 @@ fn test_that_after_release_when_no_matching_tags_exist_produces_pre_release_tag_
 
 #[rstest]
 fn test_that_after_release_when_matching_tags_exist_produces_next_pre_release_tag(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     repo.tag("v1.0.0");
     repo.commit("1.1.0+1");
@@ -101,7 +100,7 @@ fn test_that_on_release_branch_when_no_tags_exist_produces_pre_release_tag_1(mut
 
 #[rstest]
 fn test_that_on_release_branch_when_release_tags_exist_produces_pre_release_tag_1(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     repo.branch("release/1.0.0");
     repo.commit("1.0.0+1");
@@ -116,7 +115,7 @@ fn test_that_on_release_branch_when_release_tags_exist_produces_pre_release_tag_
 
 #[rstest]
 fn test_that_on_release_branch_when_previous_release_branches_exist_produces_pre_release_tag_1(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     repo.branch("release/1.0.0");
     let (sha, _) = repo.commit("1.0.0+1");
@@ -133,7 +132,7 @@ fn test_that_on_release_branch_when_previous_release_branches_exist_produces_pre
 
 #[rstest]
 fn test_that_on_release_branch_when_pre_release_tag_exist_produces_pre_release_2(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     repo.commit("0.1.0+1");
     let (sha, _) = repo.tag("v0.1.0-pre.1");
@@ -148,7 +147,7 @@ fn test_that_on_release_branch_when_pre_release_tag_exist_produces_pre_release_2
 
 #[rstest]
 fn test_that_on_release_branch_when_release_and_pre_release_tag_exist_produces_pre_release_2(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     repo.commit("0.1.0+1");
     repo.tag("v0.1.0");
@@ -164,7 +163,7 @@ fn test_that_on_release_branch_when_release_and_pre_release_tag_exist_produces_p
 
 #[rstest]
 fn test_that_on_release_branch_with_unique_version_when_pre_release_tag_exist_produces_pre_release_2(
-    mut repo: TestRepo,
+    repo: TestRepo,
 ) {
     repo.commit("0.1.0+1");
     repo.tag("v0.1.0");
